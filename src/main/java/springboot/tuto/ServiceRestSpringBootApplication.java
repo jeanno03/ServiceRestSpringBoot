141a186ca925c6d1ed49dc9c22d98b7ee24be9c2
jeannory <phou.jeannory@gmail.com>
2020-01-25 23:33:37 +0100

    20200125

--- src/main/java/springboot/tuto/ServiceRestSpringBootApplication.java
+++ src/main/java/springboot/tuto/ServiceRestSpringBootApplication.java
@@ -2,9 +2,11 @@ package springboot.tuto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServiceRestSpringBootApplication {
+    public class ServiceRestSpringBootApplication extends SpringBootServletInitializer {
+//public class ServiceRestSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRestSpringBootApplication.class, args);
